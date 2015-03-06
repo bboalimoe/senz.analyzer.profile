0618d44810c4d727a6cf5c2655b6bf3fa0aa895e
meowoodie <woodielove@163.com>
2015-03-06 19:06:25 +0800

    add model

--- static_info_site/static_info_api/model/user_device_info.py
+++ static_info_site/static_info_api/model/user_device_info.py
@@ -1,5 +1,45 @@
+import json
 from lean_cloud.lean_obj import AVObject
 
-class UserDeviceINfo(AVObject):
+class UserDeviceInfo(AVObject):
+
     def __init__(self, device_mac):
-        pass
+        self.deviceMAC = device_mac
+        device_info = self._getDeviceInfoByMAC()
+        self.userId       = device_info["userIdString"]
+        self.serialNumber = device_info["serialNumber"]
+        self.sdkVersion   = device_info["sdkVersion"]
+        self.resolution   = device_info["resolution"]
+        self.dpi          = device_info["dpi"]
+        self.abi          = device_info["abi"]
+        self.imsi         = device_info["imsi"]
+        self.hardware     = device_info["hardware"]
+        self.packageList  = self._decodePackageList(device_info["packageList"])
+
+
+
+    def _getDeviceInfoByMAC(self):
+        # Init the param
+        param = {
+            "deviceId": self.deviceMAC, # Select items which deviceId is equal to mac.
+        }
+        # Get device info
+        response = self.get(
+            where=param,       # deviceId is equal to mac in Database.
+            limit=1,           # Select 1 item of result.
+        )
+        # return json format result
+        # print json.loads(response.content)
+        return json.loads(response.content)["results"][0]
+
+
+
+    def _decodePackageList(self, package_list):
+        return package_list.split(', ')
+
+
+
+
+if __name__ == "__main__":
+
+    m = UserDeviceInfo("866707010347352")
--- static_info_site/static_info_api/model/user_profile.py
+++ static_info_site/static_info_api/model/user_profile.py
@@ -1,5 +1,50 @@
+import json
 from lean_cloud.lean_obj import AVObject
 
 class UserProfile(AVObject):
     def __init__(self, user_id):
-        pass
+        self.userId = user_id
+        user_info = self._getUserInfoByUserId()
+        self.objectId     = user_info["objectId"]
+        self.age          = user_info["age"]
+        self.hasParent    = user_info["hasParent"]
+        self.curious      = user_info["curious"]
+        self.exercise     = user_info["exercise"]
+        self.hasPet       = user_info["hasPet"]
+        self.isPregnant   = user_info["isPregnant"]
+        self.studyAbroad  = user_info["studyAbroad"]
+        self.socialManiac = user_info["socialManiac"]
+        self.education    = user_info["education"]
+        self.gender       = user_info["gender"]
+        self.shopManiac   = user_info["shopManiac"]
+        self.isSampleData = user_info["isSampleData"]
+
+
+
+    def _getUserInfoByUserId(self):
+        # Init the param
+        param = {
+            "userIdString": self.userId, # Select items which deviceId is equal to mac.
+        }
+        # Get device info
+        response = self.get(
+            where=param,       # deviceId is equal to mac in Database.
+            limit=1,           # Select 1 item of result.
+        )
+        # return json format result
+        return json.loads(response.content)["results"][0]
+
+
+
+    # PUBLIC METHOD
+    def updateUserInfo(self, **data):
+        update_data = {"objectId": self.objectId}
+        for key, value in data.iteritems():
+            update_data[key] = value
+        print update_data
+        # Update the data in Database
+        self.update_all(update_data)
+
+if __name__ == "__main__":
+
+    m = UserProfile("54d82fefe4b0d414801050ee")
