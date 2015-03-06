40e4f09b34ca51995488cbc69443c4e441f00753
meowoodie <woodielove@163.com>
2015-03-06 10:01:55 +0800

    complete the django rest api framework

---
+++ static_info_site/static_info_api/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from views import StaticInfo
+
+urlpatterns = patterns('static_info_api.views',
+    # Examples:
+    # url(r'^$', 'senz_site.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^static_info/$', StaticInfo)
+)
---
+++ static_info_site/static_info_api/views.py
@@ -0,0 +1,29 @@
+import json
+from django.http.response import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+def errorInfo():
+    import sys
+    info = "%s || %s" % (sys.exc_info()[0], sys.exc_info()[1])  # todo log the exception info
+    print info
+    return info
+
+def errorResponses(error=None):
+    if not error:
+        info = errorInfo()
+    else:
+        info = error
+
+    return JsonResponse({"status": 0}, {"errors": info})
+
+
+def successResponses(results):
+
+    return JsonResponse({"status": 1, "results": results})
+
+@csrf_exempt
+def StaticInfo(request):
+    pass
+
+
